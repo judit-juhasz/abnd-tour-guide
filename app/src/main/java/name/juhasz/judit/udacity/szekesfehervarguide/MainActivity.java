ddb2daf6898e6cde03c255519cf39f258b212f49
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-11 22:47:40 +0200

    Use tabLayout and viewpager in main activity

--- app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/MainActivity.java
+++ app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/MainActivity.java
@@ -1,5 +1,7 @@
 package name.juhasz.judit.udacity.szekesfehervarguide;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
     }
 }
