4db81d9c35aabe7016be71069eeb26fa1c8f034e
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-12 23:21:15 +0200

    Add list items to AccomodationFragment

--- app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/AccomodationFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/AccomodationFragment.java
@@ -5,9 +5,42 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class AccomodationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.local_attraction_list, container, false);
+
+        View rootView = inflater.inflate(R.layout.local_attraction_list, container, false);
+
+        ArrayList<LocalAttraction> accomodations = new ArrayList<>();
+
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_1_name),
+                getString(R.string.accomodation_1_address),
+                getString(R.string.accomodation_1_opening_hours), R.drawable.hotel_king));
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_2_name),
+                getString(R.string.accomodation_2_address),
+                getString(R.string.accomodation_2_opening_hours), R.drawable.lakeside_hotel));
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_3_name),
+                getString(R.string.accomodation_3_address),
+                getString(R.string.accomodation_3_opening_hours), R.drawable.szarcsa_hotel));
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_4_name),
+                getString(R.string.accomodation_4_address),
+                getString(R.string.accomodation_4_opening_hours), R.drawable.jak));
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_5_name),
+                getString(R.string.accomodation_5_address),
+                getString(R.string.accomodation_5_opening_hours), R.drawable.kistuba_boarding_house));
+        accomodations.add(new LocalAttraction(getString(R.string.accomodation_6_name),
+                getString(R.string.accomodation_6_address),
+                getString(R.string.accomodation_6_opening_hours), R.drawable.vadaszkurt_hotel));
+
+        LocalAttractionAdapter adapter = new LocalAttractionAdapter(getActivity(), accomodations);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        return rootView;
     }
 }
