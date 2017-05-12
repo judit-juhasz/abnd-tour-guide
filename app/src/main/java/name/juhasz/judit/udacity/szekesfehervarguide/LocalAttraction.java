f08dd3f8667ea88d194ea97d7f72a648e4069817
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-12 21:23:16 +0200

    Add custom object for storing location information

---
+++ app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/LocalAttraction.java
@@ -0,0 +1,32 @@
+package name.juhasz.judit.udacity.szekesfehervarguide;
+
+public class LocalAttraction {
+    private String mLocalAttractionName;
+    private String mLocalAttractionAddress;
+    private String mLocalAttractionOpeningHours;
+    private int mImageResourceId;
+
+    public LocalAttraction(String localAttractionName, String localAttractionAddress,
+                           String localAttractionOpeningHours, int imageResourceId) {
+        mLocalAttractionName = localAttractionName;
+        mLocalAttractionAddress = localAttractionAddress;
+        mLocalAttractionOpeningHours = localAttractionOpeningHours;
+        mImageResourceId = imageResourceId;
+    }
+
+    public String getmLocalAttractionName() {
+        return mLocalAttractionName;
+    }
+
+    public String getmLocalAttractionAddress() {
+        return mLocalAttractionAddress;
+    }
+
+    public String getmLocalAttractionOpeningHours() {
+        return mLocalAttractionOpeningHours;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+}
