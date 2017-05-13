b0feb037e51aee6ec5fcdc3d8eb7cd90e9d12bd9
Judit Juhasz <judit-juhasz@users.noreply.github.com>
2017-05-13 22:36:48 +0200

    Use more precise variable name

--- app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/AttractionFragment.java
+++ app/src/main/java/name/juhasz/judit/udacity/szekesfehervarguide/AttractionFragment.java
@@ -16,28 +16,28 @@ public class AttractionFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.local_attraction_list, container, false);
 
-        ArrayList<LocalAttraction> localAttractions = new ArrayList<>();
+        ArrayList<LocalAttraction> attractions = new ArrayList<>();
 
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_1_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_1_name),
                 getString(R.string.attraction_1_address),
                 getString(R.string.attraction_1_opening_hours), R.drawable.national_memorial_place));
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_2_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_2_name),
                 getString(R.string.attraction_2_address),
                 getString(R.string.attraction_2_opening_hours), R.drawable.orb_and_town_hall_square));
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_3_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_3_name),
                 getString(R.string.attraction_3_address),
                 getString(R.string.attraction_3_opening_hours), R.drawable.bory_castle));
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_4_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_4_name),
                 getString(R.string.attraction_4_address),
                 getString(R.string.attraction_4_opening_hours), R.drawable.st_stephen_cathedral));
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_5_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_5_name),
                 getString(R.string.attraction_5_address),
                 getString(R.string.attraction_5_opening_hours), R.drawable.open_air_museum));
-        localAttractions.add(new LocalAttraction(getString(R.string.attraction_6_name),
+        attractions.add(new LocalAttraction(getString(R.string.attraction_6_name),
                 getString(R.string.attraction_6_address),
                 getString(R.string.attraction_6_opening_hours), R.drawable.flower_clock));
 
-        LocalAttractionAdapter adapter = new LocalAttractionAdapter(getActivity(), localAttractions);
+        LocalAttractionAdapter adapter = new LocalAttractionAdapter(getActivity(), attractions);
 
         ListView listView = (ListView) rootView.findViewById(R.id.list);
 
